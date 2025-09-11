26b5ea391aab138ce808fa88fab78fe2c116b0bf
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2025-09-11 10:02:12 +0200

    show: lists

--- show.ml
+++ show.ml
@@ -1,5 +1,11 @@
 let show (type a) ~(t: a Type.ttype) (x: a) : unit =
-  let x = Obj.repr x in
-  match Type.stype_of_ttype t with
-  | Int -> print_int (Obj.obj x)
-  | String -> Printf.printf "%S" (Obj.obj x)
+  let rec show (t : Type.stype) (x : Obj.t) : unit =
+    match t with
+    | Int -> print_int (Obj.obj x)
+    | String -> Printf.printf "%S" (Obj.obj x)
+    | List t ->
+      print_char '[';
+      List.iteri (fun i x -> if i > 0 then print_string "; "; show t x) (Obj.obj x);
+      print_char ']'
+  in
+  show (Type.stype_of_ttype t) (Obj.repr x)
