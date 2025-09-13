9b6aa628ec6eaa6b2845bde837c1391c4faaabc0
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2025-09-13 16:27:24 +0200

    show: sums

--- show.ml
+++ show.ml
@@ -11,5 +11,14 @@ let show (type a) ~(t: a Type.ttype) (x: a) : unit =
       print_char '(';
       List.iteri (fun i t -> if i > 0 then print_string ", "; show t (Obj.field x i)) tl;
       print_char ')'
+    | Sum cstrs ->
+      let cst, noncst = List.partition (function (_, []) -> true | (_, _ :: _) -> false) cstrs in
+      if Obj.is_int x then begin
+        let name, _ = List.nth cst (Obj.obj x) in
+        print_string name
+      end else begin
+        let name, tl = List.nth noncst (Obj.tag x) in
+        print_string name; print_char ' '; show (Tuple tl) x
+      end
   in
   show (Type.stype_of_ttype t) (Obj.repr x)
